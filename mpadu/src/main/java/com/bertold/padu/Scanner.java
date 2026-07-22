52a675f74e30ee340adb5971961adc4537aca8eb
Bernard El-Hagin <bernard.elhagin@gmail.com>
2026-07-22 15:20:02 +0200

    Added numbers to Scanner

--- mpadu/src/main/java/com/bertold/padu/Scanner.java
+++ mpadu/src/main/java/com/bertold/padu/Scanner.java
@@ -68,7 +68,12 @@ class Scanner {
 			case '"': string(); break;
 
 			default:
-			   Padu.error(line, "Unexpected character.");
+			   if (isDigit(c)) {
+			   	number();
+			   } else {
+			   	Padu.error(line, "Unexpected character.");
+			   }
+
 			   break;
 		}
 	}
@@ -106,6 +111,30 @@ class Scanner {
 		if (isAtEnd()) return '\0';
 
 		return source.charAt(current);
+	}
+
+	private char peekNext() {
+		if (current + 1 >= source.length()) return '\0';
+
+		return source.charAt(current + 1);
+	}
+
+	private boolean isDigit(char c) {
+		return c >= '0' && c <= '9';
+	}
+
+	private void number() {
+		while (isDigit(peek())) advance(); 
+
+		// look for decimal part
+		if (peek() == '.' && isDigit(peekNext())) {
+			// consume the decimal point
+			advance();
+
+			while (isDigit(peek())) advance();
+		}
+
+		addToken(NUMBER, Double.parseDouble(source.substring(start, current)));
 	}
 
 	private boolean isAtEnd() {
