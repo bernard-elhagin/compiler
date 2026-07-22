d7f4f27d70b294238d5b458222c157a96b49827d
Bernard El-Hagin <bernard.elhagin@gmail.com>
2026-07-22 11:36:21 +0200

    Scanner does single chars

---
+++ mpadu/src/main/java/com/bertold/padu/TokenType.java
@@ -0,0 +1,49 @@
+package com.bertold.padu;
+
+enum TokenType {
+	// Single-character tokens
+	LEFT_PAREN, RIGHT_PAREN,
+	LEFT_BRACE, RIGHT_BRACE,
+	COMMA,
+	DOT,
+	MINUS,
+	PLUS,
+	SEMICOLON,
+	SLASH,
+	STAR,
+
+
+	// One or two character tokens
+	BANG, BANG_EQUAL,
+	EQUAL, EQUAL_EQUAL,
+	GREATER, GREATER_GREATER,
+	LESS, LESS_EQUAL,
+
+
+	// Literals
+	IDENTIFIER,
+	STRING,
+	NUMBER,
+
+
+	// Keywords
+	AND,
+	CLASS,
+	ELSE,
+	FALSE,
+	FUN,
+	FOR,
+	IF,
+	NIL,
+	OR,
+	PRINT,
+	RETURN,
+	SUPER,
+	THIS,
+	TRUE,
+	VAR,
+	WHILE,
+
+
+	EOF
+}
